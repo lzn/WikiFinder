88e0cab2147b4856af924974328d872748660e89
kjrz <kuba.jarz@gmail.com>
2014-12-01 18:47:21 +0100

    REST part initial commit

---
+++ wiki-finder-project/wedt/src/main/java/wedt/keywords/Html.java
@@ -0,0 +1,45 @@
+package wedt.keywords;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+import java.io.IOException;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author kjrz
+ */
+public class Html {
+
+    private final Document parsed;
+
+    public Html(String url) throws IOException {
+        this.parsed = Jsoup.connect(url).get();
+    }
+
+    public String getTitle() {
+        return parsed.title();
+    }
+
+    public List<String> getHeaders() {
+        return getByTag("h1");
+    }
+
+    public List<String> getParagraphs() {
+        return getByTag("p");
+    }
+
+    public List<String> getByTag(String tag) {
+        List<String> result = new LinkedList<>();
+        for (Element el: parsed.getElementsByTag(tag)) {
+            result.add(el.text());
+        }
+        return result;
+    }
+
+    public String getText() {
+        return parsed.body().text();
+    }
+}
---
+++ wiki-finder-project/wedt/src/main/java/wedt/rest/WikiFindingsController.java
@@ -0,0 +1,58 @@
+package wedt.rest;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import wedt.keywords.Html;
+import wedt.keywords.Keywords;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @author kjrz
+ */
+@RestController
+public class WikiFindingsController {
+
+    private static final Logger LOG = Logger.getLogger(WikiFindingsController.class);
+
+    @RequestMapping("/wiki-finder")
+    public WikiFindings wikiFind(@RequestParam(value = "url", defaultValue = "undefined") String url) {
+        LOG.info("url = " + url);
+
+        List<String> keywords = getKeywords(url);
+
+        // TODO: search keywords in Wikipedia
+
+        return writeToJson(url, keywords);
+        // TODO: write Wikipedia matches instead of keywords
+    }
+
+    private List<String> getKeywords(String url) {
+        Html html;
+        try {
+            html = new Html(url);
+        } catch (IOException | IllegalArgumentException e) {
+            LOG.warn("Failed to load url = \"" + url + "\"", e);
+            return Collections.emptyList();
+        }
+
+        LOG.info("page text: " + html.getText());
+
+        return new Keywords().find(html.getTitle());
+        // TODO: find keywords in text, not in page title
+    }
+
+    private WikiFindings writeToJson(String url, List<String> keywords) {
+        WikiFindings findings = new WikiFindings(url);
+        int i = 1;
+        for (String keyword : keywords) {
+            findings.add(i, keyword);
+            i++;
+        }
+        return findings;
+    }
+}
